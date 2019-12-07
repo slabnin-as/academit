5fbe1673ba390e7d89fa5154c6fb50dd64d07b36
Anton Slabnin <insk54@gmail.com>
2019-12-07 22:25:34 +0700

    Исправления от 07.12.2019

--- ArrayListHome/src/main/ArrayListHome.java
+++ ArrayListHome/src/main/ArrayListHome.java
@@ -7,37 +7,45 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class ArrayListHome {
-    public static void main(String[] args) throws FileNotFoundException {
-        ArrayList<String> list = new ArrayList<>();
+    public static void main(String[] args) {
+        ArrayList<String> linesFromFileList = new ArrayList<>();
 
         //считывание файла и добавление в список построчно
         try (Scanner scanner = new Scanner(new FileInputStream("input.txt"))) {
             while (scanner.hasNextLine()) {
-                list.add(scanner.nextLine());
+                linesFromFileList.add(scanner.nextLine());
             }
+        } catch (FileNotFoundException e) {
+            System.out.println("Файл не найден " + e.getMessage());
         }
-        //удаление четных чисел из списка
-        ArrayList<Integer> numList = new ArrayList<>(Arrays.asList(10, 11, 12, 13, 14, 15, 16, 17, 18, 19));
 
-        for (int i = 0; i < numList.size(); i++) {
-            if (numList.get(i) % 2 == 0) {
-                numList.remove(i);
+        for (String str : linesFromFileList) {
+            System.out.println(str);
+        }
+        System.out.println();
+
+        //удаление четных чисел из списка
+        ArrayList<Integer> numbersList1 = new ArrayList<>(Arrays.asList(10, 11, 12, 13, 14, 15, 16, 17, 18, 19));
+
+        for (int i = 0; i < numbersList1.size(); i++) {
+            if (numbersList1.get(i) % 2 == 0) {
+                numbersList1.remove(i);
                 i--;
             }
         }
-        System.out.println(numList);
+        System.out.println(numbersList1);
         System.out.println();
 
         //чистка списка от дубликатов
-        ArrayList<Integer> numList2 = new ArrayList<>(Arrays.asList(11, 11, 15, 12, 13, 11, 15, 16, 17, 12, 15));
-        ArrayList<Integer> numListNoDuplicates = new ArrayList<>();
+        ArrayList<Integer> numbersList2 = new ArrayList<>(Arrays.asList(11, 11, 15, 12, 13, 11, 15, 16, 17, 12, 15));
+        ArrayList<Integer> listWithoutDuplicateNumbers = new ArrayList<>();
 
-        for (Integer e : numList2) {
-            if (!numListNoDuplicates.contains(e)) {
-                numListNoDuplicates.add(e);
+        for (Integer e : numbersList2) {
+            if (!listWithoutDuplicateNumbers.contains(e)) {
+                listWithoutDuplicateNumbers.add(e);
             }
         }
-        System.out.println(numList2);
-        System.out.println(numListNoDuplicates);
+        System.out.println(numbersList2);
+        System.out.println(listWithoutDuplicateNumbers);
     }
 }
