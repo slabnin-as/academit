a1e38890a239096a3ac58f81635bf098263bfec8
Slabnin Anton <insk54@gmail.com>
2019-12-16 11:49:00 +0700

    Исправлена разность множеств.

--- Range/src/range/Range.java
+++ Range/src/range/Range.java
@@ -57,11 +57,11 @@ public class Range {
     public Range[] getDifference(Range range) {
         Range[] ranges;
 
-        if (range.from <= from && range.to >= to) {
+        if (range.from < from && range.to > to) {
             ranges = new Range[]{};
         } else if (from < range.from && to > range.to) {
             ranges = new Range[]{new Range(from, range.from), new Range(range.to, to)};
-        } else if (range.to >= from && range.to <= to) {
+        } else if (range.to > from && range.to < to) {
             ranges = new Range[]{new Range(range.to, to)};
         } else if (range.from > from && range.from < to) {
             ranges = new Range[]{new Range(from, range.from)};
--- Range/src/rangeapp/TestRange.java
+++ Range/src/rangeapp/TestRange.java
@@ -7,10 +7,7 @@ import java.util.Arrays;
 public class TestRange {
     public static void main(String[] args) {
         Range range1 = new Range(6, 12);
-        Range range2 = new Range(13, 20);
-
-        range1.setTo(14);
-        range2.setFrom(15);
+        Range range2 = new Range(7, 12);
 
         System.out.println(range1.getIntersection(range2));
         System.out.println(Arrays.toString(range1.getUnion(range2)));
@@ -18,5 +15,8 @@ public class TestRange {
         System.out.println(range1 + " length: " + range1.getLength());
         System.out.println("Range2 from: " + range2.getFrom() + " To: " + range2.getTo());
         System.out.println("Число 6 принадлежит диапазону range2: " + range2.isInside(6));
+
+        range1.setTo(14);
+        range2.setFrom(15);
     }
 }
