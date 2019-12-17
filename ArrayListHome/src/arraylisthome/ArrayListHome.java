3409eed8581cc40a76b422f63f566aa697717414
Slabnin Anton <insk54@gmail.com>
2019-12-17 07:03:28 +0700

    Переименованы пакеты в модулях ArrayListHome, MyList, Shape

--- ArrayListHome/src/arraylisthome/ArrayListHome.java
+++ ArrayListHome/src/arraylisthome/ArrayListHome.java
@@ -1,4 +1,4 @@
-package main;
+package arraylisthome;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
--- Shape/src/shapesmain/Main.java
+++ Shape/src/shapesmain/Main.java
@@ -1,4 +1,4 @@
-package main;
+package shapesmain;
 
 import shapes.*;
 
