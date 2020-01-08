e33ef8e32becf9a958242cd95ffa331c3ba4ec2e
Slabnin Anton <insk54@gmail.com>
2020-01-09 05:32:55 +0700

    Парсер CSV версия 0.92

--- Csv/src/csvparser/ReadCsv.java
+++ Csv/src/csvparser/ReadCsv.java
@@ -3,44 +3,77 @@ package csvparser;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ReadCsv {
-    public static void main(String[] args) {
-        try (Scanner scanner = new Scanner(new FileInputStream("input.csv")); PrintWriter writer = new PrintWriter("index.html")) {
-            while (scanner.hasNextLine()) {
-                final char quotes = '\"';
-                final char separator = ',';
-                String cellStr = null;
-                int separatorIndex;
-                int doubleQuoteIndex;
+    private static String quotes = "\"";
 
+    public static void main(String[] args) {
+        String cellsLineSeparator = "<br/>";
+
+        try (Scanner scanner = new Scanner(new FileInputStream("input.csv")); PrintWriter writer = new PrintWriter("index.html")) {
+            writer.println("<!DOCTYPE html>");
+            writer.println("<html>");
+            writer.println("<head>");
+            writer.println("<meta charset=\"UTF-8\">");
+            writer.println("<title>Парсер CSV</title>");
+            writer.println("</head>");
+            writer.println("<body>");
+            writer.println("<table border=\"1\">");
+
+            while (scanner.hasNextLine()) {
                 String line = scanner.nextLine();
-                if (line.charAt(0) != quotes) {
-                    separatorIndex = line.indexOf(separator);
-                    if (separatorIndex > 0) {
-                        cellStr = line.substring(0, separatorIndex);
+                String[] splitText = line.split(",");
+                ArrayList<String> cells = new ArrayList<>();
+
+                for (int i = 0; i < splitText.length; i++) {
+                    if (readNextLine(splitText[i])) {
+                        line = line + cellsLineSeparator + scanner.nextLine();
+                        splitText = line.split(",");
+                    }
+                    if (isPrevCellsPart(splitText[i])) {
+                        cells.set(cells.size() - 1, cells.get(cells.size() - 1) + "," + splitText[i]);
                     } else {
-                        cellStr = line;
+                        cells.add(splitText[i]);
                     }
                 }
-                if (line.charAt(0) == quotes) {
-                    doubleQuoteIndex = line.indexOf("\"\"");
-                    if (doubleQuoteIndex > 0) {
-                        separatorIndex = line.indexOf("\",", doubleQuoteIndex+2);
-                    } else {
-                        separatorIndex = line.indexOf("\",");
+
+                writer.println("<tr>");
+                for (String cell : cells) {
+                    if (cell.charAt(0) == '\"') {
+                        cell = clearQuotes(cell);
                     }
-                    if (separatorIndex > 0) {
-                        cellStr = line.substring(1, separatorIndex);
-                    } else {
-                        cellStr = line;
-                    }
+                    cell = cell.replaceAll("\"\"", quotes);
+                    writer.println("<td>");
+                    writer.println(cell);
+                    writer.println("</td>");
                 }
-                System.out.println(cellStr);
+                writer.println("</tr>");
             }
+
+            //конец HTML файла
+            writer.println("</table>");
+            writer.println("</body>");
+            writer.println("</html>");
+
         } catch (FileNotFoundException e) {
             System.out.println("Файл не найден " + e.getMessage());
         }
+    }
+
+    //проверка что следующий кусок после разделения является частью предыдущего
+    private static boolean isPrevCellsPart(String text) {
+        return text.indexOf(quotes) == text.lastIndexOf(quotes) && text.endsWith(quotes);
+    }
+
+    //проверка на перенос строки внутри ячейки
+    private static boolean readNextLine(String text) {
+        return text.startsWith(quotes) && !text.endsWith(quotes);
+    }
+
+    //удаление кавычек ограничителей строки
+    private static String clearQuotes(String text) {
+        return text.substring(1, text.length() - 1);
     }
 }
