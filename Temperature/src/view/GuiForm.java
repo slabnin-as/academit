1a20d73fdba66140666a612cdcb93f8b811b68b4
Slabnin Anton <insk54@gmail.com>
2020-01-14 09:42:31 +0700

    SwingUtilities add

--- Temperature/src/view/GuiForm.java
+++ Temperature/src/view/GuiForm.java
@@ -16,57 +16,59 @@ public class GuiForm {
     private JButton rbConvert;
 
     public GuiForm(Scale[] scales) {
-        JFrame frame = new JFrame("Конвертер температур");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        SwingUtilities.invokeLater(() -> {
+            JFrame frame = new JFrame("Конвертер температур");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        JPanel contentPane = new JPanel();
-        contentPane.setLayout(new GridLayout(3, 1, 5, 5));
+            JPanel contentPane = new JPanel();
+            contentPane.setLayout(new GridLayout(3, 1, 5, 5));
 
-        JPanel topPanel = new JPanel();
-        topPanel.setLayout(new FlowLayout(FlowLayout.LEFT, 15, 5));
+            JPanel topPanel = new JPanel();
+            topPanel.setLayout(new FlowLayout(FlowLayout.LEFT, 15, 5));
 
-        JLabel lbInputScale = new JLabel("Выберите шкалу");
-        lbInputScale.setFont(new Font(lbInputScale.getFont().getName(), Font.BOLD, 14));
-        inputScale = new JComboBox<>(scales);
-        inputScale.setFont(new Font(inputScale.getFont().getName(), Font.BOLD, 14));
-        inputTemp = new JTextField(5);
-        inputTemp.setFont(new Font(inputTemp.getFont().getName(), Font.BOLD, 16));
+            JLabel lbInputScale = new JLabel("Выберите шкалу");
+            lbInputScale.setFont(new Font(lbInputScale.getFont().getName(), Font.BOLD, 14));
+            inputScale = new JComboBox<>(scales);
+            inputScale.setFont(new Font(inputScale.getFont().getName(), Font.BOLD, 14));
+            inputTemp = new JTextField(5);
+            inputTemp.setFont(new Font(inputTemp.getFont().getName(), Font.BOLD, 16));
 
-        topPanel.add(lbInputScale);
-        topPanel.add(inputScale);
-        topPanel.add(inputTemp);
+            topPanel.add(lbInputScale);
+            topPanel.add(inputScale);
+            topPanel.add(inputTemp);
 
-        JPanel midPanel = new JPanel();
-        midPanel.setLayout(new FlowLayout(FlowLayout.LEFT, 15, 5));
+            JPanel midPanel = new JPanel();
+            midPanel.setLayout(new FlowLayout(FlowLayout.LEFT, 15, 5));
 
-        JLabel lbOutputScale = new JLabel("Выберите шкалу");
-        lbOutputScale.setFont(new Font(lbOutputScale.getFont().getName(), Font.BOLD, 14));
-        outScale = new JComboBox<>(scales);
-        outScale.setFont(new Font(outScale.getFont().getName(), Font.BOLD, 14));
-        lbResult = new JLabel();
-        lbResult.setFont(new Font(lbResult.getFont().getName(), Font.BOLD, 18));
+            JLabel lbOutputScale = new JLabel("Выберите шкалу");
+            lbOutputScale.setFont(new Font(lbOutputScale.getFont().getName(), Font.BOLD, 14));
+            outScale = new JComboBox<>(scales);
+            outScale.setFont(new Font(outScale.getFont().getName(), Font.BOLD, 14));
+            lbResult = new JLabel();
+            lbResult.setFont(new Font(lbResult.getFont().getName(), Font.BOLD, 18));
 
-        midPanel.add(lbOutputScale);
-        midPanel.add(outScale);
-        midPanel.add(lbResult);
+            midPanel.add(lbOutputScale);
+            midPanel.add(outScale);
+            midPanel.add(lbResult);
 
-        JPanel botPanel = new JPanel();
-        botPanel.setLayout(new FlowLayout(FlowLayout.RIGHT));
+            JPanel botPanel = new JPanel();
+            botPanel.setLayout(new FlowLayout(FlowLayout.RIGHT));
 
-        rbConvert = new JButton("Перевести");
-        rbConvert.setFont(new Font(rbConvert.getFont().getName(), Font.BOLD, 14));
+            rbConvert = new JButton("Перевести");
+            rbConvert.setFont(new Font(rbConvert.getFont().getName(), Font.BOLD, 14));
 
-        botPanel.add(rbConvert);
+            botPanel.add(rbConvert);
 
-        contentPane.add(topPanel);
-        contentPane.add(midPanel);
-        contentPane.add(botPanel);
+            contentPane.add(topPanel);
+            contentPane.add(midPanel);
+            contentPane.add(botPanel);
 
-        frame.setContentPane(contentPane);
-        frame.pack();
-        frame.setResizable(false);
-        frame.setLocationRelativeTo(null);
-        frame.setVisible(true);
+            frame.setContentPane(contentPane);
+            frame.pack();
+            frame.setResizable(false);
+            frame.setLocationRelativeTo(null);
+            frame.setVisible(true);
+        });
     }
 
     private JComboBox<Scale> getInputScale() {
@@ -100,11 +102,9 @@ public class GuiForm {
         Scale inputScale = (Scale) getInputScale().getSelectedItem();
         Scale resultScale = (Scale) getOutputScale().getSelectedItem();
 
-        Converter converter;
-
         if (checkUserInput()) {
             double inputTemperature = Double.parseDouble(inputTemp.getText());
-            converter = new Converter(inputScale, resultScale, inputTemperature);
+            Converter converter = new Converter(inputScale, resultScale, inputTemperature);
             double resultTemperature = converter.convert();
 
             lbResult.setText(DecimalFormat.getNumberInstance(Locale.ENGLISH).format(resultTemperature));
