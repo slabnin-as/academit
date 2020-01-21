d71c23a0bfd9b6b2bad6e484e9efce79655e98e0
Slabnin Anton <insk54@gmail.com>
2020-01-21 11:01:04 +0700

    Удален контроллер. Убран метод checkUserInput, температура передается через аргумент. Убраны сокращения в именах

--- Temperature/src/app/TemperatureConverter.java
+++ Temperature/src/app/TemperatureConverter.java
@@ -6,6 +6,6 @@ import view.GuiForm;
 public class TemperatureConverter {
     public static void main(String[] args) {
         Scale[] scales = {new CelsiusScale(), new KelvinScale(), new FahrenheitScale()};
-        GuiForm guiForm = new GuiForm(scales);
+        new GuiForm(scales);
     }
 }
